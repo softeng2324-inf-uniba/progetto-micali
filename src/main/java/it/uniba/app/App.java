8108128d7ff2526a2c8d568dd05c1f263adc65dc
Michele_Russo <132654608+MikeRvsso@users.noreply.github.com>
2024-04-11 10:11:13 +0200

    Update App.java

--- src/main/java/it/uniba/app/App.java
+++ src/main/java/it/uniba/app/App.java
@@ -11,7 +11,7 @@ public final class App {
      * @return the "Hello World!" string.
      */
     public String getGreeting() {
-        return "Hello World!!\n TestPackage";
+        return "Hello World!!";
         
 
     }
