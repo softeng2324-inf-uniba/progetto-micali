d86e00f4a2e9d7b08c72452e9b367a3308a426c1
MikeRvsso <michelerusso141@gmail.com>
2024-05-10 11:50:53 +0200

    Creazione modulo handleHelp che stampa i comandi disponibili

--- src/main/java/it/uniba/app/App.java
+++ src/main/java/it/uniba/app/App.java
@@ -1,9 +1,12 @@
 package it.uniba.app;
 
-
+import java.io.IOException;
+import java.util.Scanner;
 
 
 import javax.annotation.Syntax;
+
+import it.uniba.app.features.CommandType;
 
 /**
  * Main class of the application.
@@ -24,5 +27,17 @@ public final class App {
 
     public static void main(final String[] args) {
         System.out.println(getGreeting()); 
+    }
+
+
+        public static void handleHelp(final Scanner input , final Scanner value , final CommandType command) throws IOException {
+        System.out.println("Comandi disponibili: ");
+        System.out.println("/gioca - Inizia una nuova partita");
+        System.out.println("/esci - Termina il gioco");
+        System.out.println("/aiuto - Mostra l'elenco dei comandi disponibili");
+        System.out.println("/vuoto - Crea un tavoliere vuoto");
+        System.out.println("/tavoliere - Mostra il tavoliere di gioco");
+        System.out.println("/qualimosse - Mostra le mosse disponibili");
+        System.out.println("/abbandona - Abbandona la partita in corso");
     }
 }
