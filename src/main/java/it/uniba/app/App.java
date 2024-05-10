8e8efcd7d67b26f4b3abb74269c7e96fe4832f76
MikeRvsso <michelerusso141@gmail.com>
2024-05-10 11:59:32 +0200

    Creazione della mappa dei comandi con i vari moduili che successivamente andremo a sviluppare

--- src/main/java/it/uniba/app/App.java
+++ src/main/java/it/uniba/app/App.java
@@ -1,19 +1,25 @@
 package it.uniba.app;
 
+import it.uniba.app.features.ColorShell;
+import it.uniba.app.features.ViewResult;
+import it.uniba.app.interfaces.HandleModule;
 import java.io.IOException;
 import java.util.Scanner;
-
-
-import javax.annotation.Syntax;
-
+import java.io.IOException;
 import it.uniba.app.features.CommandType;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.regex.Pattern;
+import javax.annotation.Syntax;
 
 /**
  * Main class of the application.
  */
 public final class App {
 
-    
+    public static boolean exit = false;   
+    public static final Map<CommandType, HandleModule> COMMAND = new HashMap<>();
+
 
 
     /**
@@ -25,9 +31,56 @@ public final class App {
         return "Hello World!!";
     }
 
-    public static void main(final String[] args) {
-        System.out.println(getGreeting()); 
+     public static String getMessage(final ViewResult msg) {
+        return msg.getMessage();
     }
+
+    public static void printWelcome(final ColorShell color) {
+        System.out.print(getMessage(ViewResult.WELCOME));
+        System.out.println("/help per avere un aiuto !");
+    }
+
+            /**
+     * Metodo che controlla se il comando inserito dall'utente è presente tra i
+     * comandi disponibili.
+     *
+     * @param commands Mappa dei comandi Map<CommandType, CommandHandler>
+     * @param value    comando inserito dall'utente
+     *
+     * @return il comando inserito dall'utente se alias corretto | null altrimenti
+     */
+    public static CommandType checkCommand(final Map<CommandType, HandleModule> commands, final String value) {
+        for (CommandType iteration : commands.keySet()) {
+            for (Pattern patt : iteration.getCommandPattern()) {
+                if (patt.matcher(value).matches()) {
+                    return iteration;
+                }
+            }
+        }
+        return null;
+    }
+
+    /** 
+     * Metodo che crea la mappa dei comandi disponibili per il gioco.
+     * @return Map<String, CommandType>
+     */
+    static {
+        COMMAND.put(CommandType.HELP,  App::handleHelp);
+        //COMMAND.put(CommandType.EXIT,  App::handleExit);
+        //COMMAND.put(CommandType.START, App::handlePlay);
+        //COMMAND.put(CommandType.TABLE, App::handleBoard);
+        //COMMAND.put(CommandType.EMPTY, App::handleEmpty);
+        //COMMAND.put(CommandType.MOVES, App::handleMoves);
+       }
+
+
+    public static void main(final String[] args) {
+        printWelcome(ColorShell.RED);
+
+    }
+
+
+
 
 
         public static void handleHelp(final Scanner input , final Scanner value , final CommandType command) throws IOException {
